55c047eb162aa5776a857513bc64944b1c9ea523
MarcelEntner <marcel.entner@sz-ybbs.ac.at>
2022-03-07 13:17:02 +0100

    layout gerüst hinzugefügt

---
+++ SocialMediaTool/resources/views/Layout/websitelayout.blade.php
@@ -0,0 +1,43 @@
+<html lang="at">
+
+<head>
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible">
+    <!-- CSS only  Bootstrap-->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>@yield('title')</title> <!-- Platzhalter für den Title , Title steht in der Variable title-->
+   
+</head>
+
+<body>
+    @section('header')
+ 
+
+    @show
+
+
+
+    @section('content')
+
+
+    @show
+
+
+
+    @section('footer')
+
+
+        
+
+    @show
+
+
+
+
+
+
+</body>
+
+</html>
