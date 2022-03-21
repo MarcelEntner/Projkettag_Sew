7534238f7d6df29487bd904c70c1908ada2cfa35
SimonKerschbaummayr <simon.kerschbaummayr@sz-ybbs.ac.at>
2022-03-21 14:28:12 +0100

    MainPage

--- SocialMediaTool/resources/views/layouts/app.blade.php
+++ SocialMediaTool/resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Social Media Tool') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
--- SocialMediaTool/resources/views/mainpage.blade.php
+++ SocialMediaTool/resources/views/mainpage.blade.php
@@ -1,7 +1,58 @@
 @extends('layouts.app')
-
 @section('content')
-<div>
-        
-</div>
+<div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm border-primary">
+          <div class="card-header py-3 text-white bg-primary border-primary">
+            <h4 class="my-0 fw-normal">Youtube</h4>
+          </div>
+          <div class="card-body">
+            <img style="width: 10vw; heigth: 10vh" src="../storage/images/youtube.png">
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm border-primary">
+          <div class="card-header py-3 text-white bg-primary border-primary">
+            <h4 class="my-0 fw-normal">Instagram</h4>
+          </div>
+          <div class="card-body">
+            <img style="width: 10vw; heigth: 10vh" src="../storage/images/instagram.png">
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm border-primary">
+          <div class="card-header py-3 text-white bg-primary border-primary">
+            <h4 class="my-0 fw-normal">Twitter</h4>
+          </div>
+          <div class="card-body">
+            <img style="width: 10vw; heigth: 10vh" src="../storage/images/twitter.png">
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm border-primary">
+          <div class="card-header py-3 text-white bg-primary border-primary">
+            <h4 class="my-0 fw-normal">Facebook</h4>
+          </div>
+          <div class="card-body">
+            <img style="width: 10vw; heigth: 10vh" src="../storage/images/facebook.png">
+          </div>
+        </div>
+      </div>
+    </div>
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-muted">© 2022 Social Media Tool</p>
+
+    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+    </a>
+
+    <ul class="nav col-md-4 justify-content-end">
+      <li class="nav-item"><a href="{{ route('main') }}" class="nav-link px-2 text-muted">Home</a></li>
+      <li class="nav-item"><a href="{{ route('main') }}" class="nav-link px-2 text-muted">CreatePost</a></li>
+      <li class="nav-item"><a href="{{ route('main') }}" class="nav-link px-2 text-muted">Platform-login</a></li>
+    </ul>
+  </footer>
 @endsection
